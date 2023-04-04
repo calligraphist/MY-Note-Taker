29e90c627a1ba3e7b4fcc4a64c8589a19096dce3
“anar <sehar.uzair23@gmail.com>
2023-04-03 21:22:55 -0300

    server.js added

---
+++ server.js
@@ -0,0 +1,9 @@
+// Import Express.js
+const express = require('express');
+
+// Initialize an instance of Express.js
+const app = express();      
+
+// Specify on which port the Express.js server will run
+const PORT = 3001;
+
