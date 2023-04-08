4f594e7b4800d978cd69e21796c0d43f52208924
“anar <sehar.uzair23@gmail.com>
2023-04-08 16:40:37 -0300

    routes completed

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -8,18 +8,18 @@ const getNotes = () => {
 router.get('/notes', (req, res) => {
   getNotes().then(notes => res.json(notes))
 });
+//post notes is available
 router.post('/notes', (req, res) => {
   getNotes().then(oldNotes => {
     const newNotes = [...oldNotes, { title: req.body.title, text: req.body.text, id: Math.floor(Math.random() * 1000).toString() }]
     writeFile('db/db.json', JSON.stringify(newNotes)).then(() => res.json({ msg: 'ok' }))
   })
 })
+//notes can be deleted
 router.delete('/notes/:id', (req, res) => {
   getNotes().then(oldNotes => {
-
     const updatedNotes = oldNotes.filter(note => note.id !== req.params.id)
     writeFile('db/db.json', JSON.stringify(updatedNotes)).then(() => res.json({ msg: 'ok' }))
-
   })
 })
 
